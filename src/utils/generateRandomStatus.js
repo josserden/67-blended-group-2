32355c399acde3492a81167a4d88f2696e4bfcb6
Den <52295780+josserden@users.noreply.github.com>
2023-04-08 23:00:02 +0300

    upd main

--- src/utils/generateRandomStatus.js
+++
@@ -1,6 +0,0 @@
-export const generateRandomStatus = () => {
-  const statuses = ['online', 'offline', 'busy'];
-  const randomIndex = Math.floor(Math.random() * statuses.length);
-
-  return statuses[randomIndex];
-};
